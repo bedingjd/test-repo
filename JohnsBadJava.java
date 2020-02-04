ea3a6527df81b0d499ddc0f7eeb65ad0625c835b
John Bedingfield <john.bedingfield@yahoo.com>
2020-02-04 12:18:00 -0600

    added a bad java file, modified

---
+++ JohnsBadJava.java
@@ -0,0 +1,8 @@
+// something is defintely wrong with this code
+
+class JohnsBadJava{
+	public static void main(String[] args){
+		// I think the problem is here
+	Systtem.out.println("Hello, World!);
+	}
+}
